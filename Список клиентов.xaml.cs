b9ccf073a7eaa55227b6e18b3db0b64c3841fff2
Danya0211 <102015087+Danya0211@users.noreply.github.com>
2022-03-30 09:40:24 +0300

    Add files via upload

---
+++ Список клиентов.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Marquize01
+{
+    /// <summary>
+    /// Логика взаимодействия для Список_клиентов.xaml
+    /// </summary>
+    public partial class Список_клиентов : Window
+    {
+        public Список_клиентов()
+        {
+            InitializeComponent();
+        }
+
+        private void Фамилия_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
